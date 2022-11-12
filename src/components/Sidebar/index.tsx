d1d527f627e9bc140648f872f654e2a2e1002e25
Antonio <antonio.jpsalves@gmail.com>
2022-11-11 23:09:41 -0300

    Ajustando o estilo do sidebar, adicionando a lib para utilizar icon e profile pic

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,16 +1,22 @@
 import styles from './styles.module.css';
 
+import { PencilLine } from 'phosphor-react';
+
 export function Sidebar() { 
     return (
         <aside className={styles.sidebar}>
             <img src="https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" className={styles.cover} />
 
             <div className={styles.profile}>
+                <img src="https://github.com/antoniojpsalves.png" className={styles.avatar} alt="Foto do perfil" />
                 <strong>Antonio Alves</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     );
